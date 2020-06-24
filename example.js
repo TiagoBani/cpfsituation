81f7b5f3b799397b0dd31981152b4f4c8fd64819
Tiago Bani <tiago.bani@yahoo.com.br>
2020-06-24 15:12:37 -0300

    update example.js

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 (async () => {
     const config = require('./config.json')
     const situation = require('./src/index')
-
     const results = await situation({ validate: config.validate });
     console.log(results)
 })();
